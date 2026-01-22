59dfda1cde1d899fa645b25b6308b7f449f51618
olliesmith6121-ops <olliesmith6121@googlemail.com>
2026-01-22 12:33:36 +0000

    UI updates

--- server.ps1
+++ server.ps1
@@ -1,39 +1,40 @@
+$port = 8080
 $listener = New-Object System.Net.HttpListener
-$listener.Prefixes.Add("http://localhost:8080/")
+$listener.Prefixes.Add("http://localhost:$port/")
 $listener.Start()
-Write-Host "Server listening on http://localhost:8080/"
-
-$mimeTypes = @{
-    ".html" = "text/html"
-    ".css"  = "text/css"
-    ".js"   = "application/javascript"
-    ".json" = "application/json"
-    ".png"  = "image/png"
-    ".jpg"  = "image/jpeg"
-    ".svg"  = "image/svg+xml"
-}
+Write-Host "Listening on http://localhost:$port/"
 
 try {
     while ($listener.IsListening) {
         $context = $listener.GetContext()
         $request = $context.Request
         $response = $context.Response
-
+        
         $path = $request.Url.LocalPath
         if ($path -eq "/") { $path = "/index.html" }
-        
-        # Remove leading slash and convert to local path
-        $localPath = Join-Path $PWD ($path.TrimStart("/"))
+        $localPath = Join-Path (Get-Location) $path
         
         if (Test-Path $localPath -PathType Leaf) {
-            $content = [System.IO.File]::ReadAllBytes($localPath)
-            $extension = [System.IO.Path]::GetExtension($localPath)
-            if ($mimeTypes.ContainsKey($extension)) {
-                $response.ContentType = $mimeTypes[$extension]
+            try {
+                $content = [System.IO.File]::ReadAllBytes($localPath)
+                
+                $extension = [System.IO.Path]::GetExtension($localPath)
+                switch ($extension) {
+                    ".html" { $response.ContentType = "text/html" }
+                    ".css"  { $response.ContentType = "text/css" }
+                    ".js"   { $response.ContentType = "application/javascript" }
+                    ".png"  { $response.ContentType = "image/png" }
+                    ".jpg"  { $response.ContentType = "image/jpeg" }
+                    ".json" { $response.ContentType = "application/json" }
+                    ".svg"  { $response.ContentType = "image/svg+xml" }
+                }
+                
+                $response.ContentLength64 = $content.Length
+                $response.OutputStream.Write($content, 0, $content.Length)
+                $response.StatusCode = 200
+            } catch {
+                $response.StatusCode = 500
             }
-            $response.ContentLength64 = $content.Length
-            $response.OutputStream.Write($content, 0, $content.Length)
-            $response.StatusCode = 200
         } else {
             $response.StatusCode = 404
         }
